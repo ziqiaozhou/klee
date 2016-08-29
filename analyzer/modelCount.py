65d265cdc4d2fcc00b04f3cb1ee02a579c58000f
Ziqiao Zhou <ziqiao@brawn.cs.unc.edu>
2016-08-29 17:32:28 -0400

    model count

---
+++ analyzer/modelCount.py
@@ -0,0 +1,13 @@
+import numpy;
+
+def solve(pcfile):
+    result=subprocess.check_output(runcommand,shell=True)
+
+def BSAT(pcfile,pivot,r,wmax,S):
+    wmin=wmax/r
+    wtotal=0
+    Y=set([])
+    while wtotal/(wmin*r)<=pivot:
+        y=solve(pcfile)
+
+    
