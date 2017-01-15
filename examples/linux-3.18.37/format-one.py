eddcbc9fea96addde15d044434cbe232bdc05c3e
ziqiaozhou <ziqiao@cs.unc.edu>
2017-01-15 13:16:02 -0500

    divide pc into groups and run kleaver

--- examples/linux-3.18.37/format-one.py
+++ examples/linux-3.18.37/format-one.py
@@ -9,19 +9,21 @@ for filename in os.listdir("./"):
 total=len(flist)
 onesum=500
 par=total/onesum
+#par=6
 cmd=""
 finalfile=[]
 for i in range(0,par):
-    sublist=flist[i*onesum:max((1+i)*onesum,total-1)]
-    linkfile='--link-pc-file='+' --link-pc-file='.join(sublist[1:])+' '
-    cmd=cmd+"\n nohup "+" kleaver --evaluate-or --out=result.subpc"+str(i)+" "+linkfile+sublist[0]+" &"
-    finalfile.append("result.subpc"+str(i))
+	if i*onesum > (total-1):
+		break;
+	sublist=flist[i*onesum:min((1+i)*onesum,total-1)]
+	linkfile='--link-pc-file='+' --link-pc-file='.join(sublist[1:])+' '
+	cmd=cmd+"\n nohup "+" kleaver --evaluate-or --out=result.subpc"+str(i)+" "+linkfile+sublist[0]+" &"
+	finalfile.append("result.subpc"+str(i))
 
 #cmd+=cmd+"\n"+"echo wait finished;"
 
-cmd2="kleaver --evaluate-or --out=result.pc  --link-pc-file="+'--link-pc-file'.join(finalfile[1:])+" "+finalfile[0]
+cmd2="kleaver --evaluate-or --out=result.pc  --link-pc-file="+' --link-pc-file='.join(finalfile[1:])+" "+finalfile[0]
 
-f=open('cmd.txt',"w+")
+f=open('cmd16.txt',"w+")
 f.write(cmd+"\n"+cmd2)
-
 os.system(cmd+"\n"+cmd2);
